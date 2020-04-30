ce81abfdcfc9828ecfe6033938c2c96abf1d1dd5
Yuta Saito <kateinoigakukun@gmail.com>
2020-04-30 13:31:23 +0900

    Add ci script

---
+++ .ci/netlify.sh
@@ -0,0 +1,11 @@
+TOOLCHAIN_NAME="$(cat .swift-version)"
+TOOLCHAIN_DOWNLOAD="https://github.com/swiftwasm/swift/releases/download/swift-$TOOLCHAIN_NAME/swift-$TOOLCHAIN_NAME-linux.tar.gz"
+
+git clone https://github.com/kylef/swiftenv.git ~/.swiftenv
+export SWIFTENV_ROOT="$HOME/.swiftenv"
+export PATH="$SWIFTENV_ROOT/bin:$PATH"
+eval "$(swiftenv init -)"
+swiftenv install $TOOLCHAIN_DOWNLOAD
+
+npm install
+npm build:prod
